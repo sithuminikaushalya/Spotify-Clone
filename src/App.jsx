f352a31568f52fae871c44a47142ee9f4afa09cd
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-07-27 11:18:18 +0530

    UI for player is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Sidebar } from './components/Sidebar'
+import Player from './components/Player'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+      <Player/>
+    </div>
   )
 }
 
