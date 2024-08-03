caf55a1dcee4a76e5e4385606fd3e2b7efe23e9e
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-08-03 23:44:52 +0530

    successfully finish all the main function with connection with backend and databse

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,9 +7,9 @@ const Player = () => {
 
     const { seekBar, seekBg, playStatus, play, pause, track, time, previous, next, seekSong } = useContext(PlayerContext);
 
-    return (
+    return track ? (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
-            <div className='hidden lg:flex items-center gap-4'>
+            <div className='items-center hidden gap-4 lg:flex'>
                 <img className='w-12' src={track.image} alt='' />
                 <div>
                     <p>{track.name}</p>
@@ -31,25 +31,25 @@ const Player = () => {
                 <div className='flex items-center gap-5'>
                     <p>{time.currentTime.minute}:{time.currentTime.second}</p>
                     <div ref={seekBg} onClick={seekSong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-                        <hr ref={seekBar} className='h-1 border-none w-10 bg-green-800 rounded-full' />
+                        <hr ref={seekBar} className='w-10 h-1 bg-green-800 border-none rounded-full' />
                     </div>
                     <p>{time.totalTime.minute}:{time.totalTime.second}</p>
                 </div>
             </div>
-            <div className='hidden lg:flex items-center gap-2 opacity-75'>
+            <div className='items-center hidden gap-2 opacity-75 lg:flex'>
                 <img className='w-4' src={assets.plays_icon} alt="" />
                 <img className='w-4' src={assets.mic_icon} alt="" />
                 <img className='w-4' src={assets.queue_icon} alt="" />
                 <img className='w-4' src={assets.speaker_icon} alt="" />
                 <img className='w-4' src={assets.volume_icon} alt="" />
-                <div className='w-20 bg-slate-50 h-1 rounded'>
+                <div className='w-20 h-1 rounded bg-slate-50'>
 
                 </div>
                 <img className='w-4' src={assets.mini_player_icon} alt="" />
                 <img className='w-4' src={assets.zoom_icon} alt="" />
             </div>
         </div>
-    )
+    ) : null
 }
 
 export default Player
