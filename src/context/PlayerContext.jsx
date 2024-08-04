aac358a5e0267be281acef4bfa1979f51d80c4fa
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-08-04 10:22:59 +0530

    add hosted backend url

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -10,7 +10,7 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
-    const url = 'http://localhost:4000';
+    const url = 'https://spotify-backend-livid.vercel.app/';
 
     const [songsData,setSongsData] = useState([]);
     const [albumsData,setAlbumsData] = useState([]);
